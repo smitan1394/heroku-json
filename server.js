f344525b7174ace99f97212a43e5d66dac5b1944
smitan1394 <42381355+smitan1394@users.noreply.github.com>
2019-06-25 17:58:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const jsonServer = require('json-server');
 var express = require('express');
+const path = require('path');
 var server = express();
+
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
-
+server.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + `index.html`)); // load the single view file (angular will handle the page changes on the front-end)
+});
 server.listen(port);
 
